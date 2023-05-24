992fb1b2e79e91cd7163e01bc7239d1aec28f5f4
rizan1998 <rizanalfalah@gmail.com>
2023-05-24 21:46:49 +0700

    setup project react, install tailwind css and create necessarry folder

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-400 text-5xl"> testing</h1>
+    </div>
+  );
+}
+
+export default App;
