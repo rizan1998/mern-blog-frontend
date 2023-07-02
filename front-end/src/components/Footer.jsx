81824a89b969a5d7c05313c1a414f62cbf574346
rizan1998 <rizanalfalah@gmail.com>
2023-07-02 20:43:26 +0700

    menambah footer mobile dan tablet device desktop unfinished

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,7 +1,128 @@
 import React from "react";
+import { AiOutlineTwitter, AiFillInstagram, AiFillHeart } from "react-icons/ai";
+import { FaFacebook, FaGithub } from "react-icons/fa";
+import { BsTelegram } from "react-icons/bs";
+
+import { images } from "../constants";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <section className="bg-dark-hard">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 ">
+        <div className="col-span-5 md:col-span-4 ">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">Landingpage</a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">Referral Program</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5 md:col-span-4 ">
+          <h3 className="text-dark-light font-bold md:text-lg">Services</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">Design</a>
+            </li>
+            <li>
+              <a href="/">Themes</a>
+            </li>
+            <li>
+              <a href="/">Ilustrations</a>
+            </li>
+            <li>
+              <a href="/">UI Kit</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5 md:col-span-4 md:col-start-5 ">
+          <h3 className="text-dark-light font-bold md:text-lg">Company</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">about</a>
+            </li>
+            <li>
+              <a href="/">Terms</a>
+            </li>
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">Referral Program</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5 md:col-span-4 ">
+          <h3 className="text-dark-light font-bold md:text-lg">More</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">License</a>
+            </li>
+            <li>
+              <a href="/">Changelog</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-10 md:order-first md:col-span-4">
+          <img src={images.logo} alt="logo" className="brightness-0 invert mx-auto md:mx-0" />
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base">Build a modern and creative website with cleans</p>
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
+            <li>
+              <a href="">
+                <FaFacebook className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaGithub className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <BsTelegram className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <AiOutlineTwitter className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <AiFillInstagram className="w-6 h-auto" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12">
+          <div className="bg-primary text-white p-3 rounded-full">
+            <AiFillHeart className="w-7 h-auto" />
+          </div>
+          <p className="font-bold italic text-dark-light ">Copyright © 2023. Crafted with love.</p>
+        </div>
+      </footer>
+    </section>
+  );
 };
 
 export default Footer;
