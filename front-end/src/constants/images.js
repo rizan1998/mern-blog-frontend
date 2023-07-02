19d2a5b1e01f83e07cd55467a5f91129a30c70e1
rizan1998 <rizanalfalah@gmail.com>
2023-07-02 15:06:07 +0700

    add the articles section

--- front-end/src/constants/images.js
+++ front-end/src/constants/images.js
@@ -1,8 +1,12 @@
 import logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
+import Post1Image from "../assets/posts/posts1.jpg";
+import PostProfileImage from "../assets/posts/posts-profile.svg";
 const images = {
   logo,
   HeroImage,
+  Post1Image,
+  PostProfileImage,
 };
 
 export default images;
