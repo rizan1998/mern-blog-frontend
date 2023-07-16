c0c4475c2ccf02b2ee9bdc28e83ce6fe1691bfcc
rizan1998 <rizanalfalah@gmail.com>
2023-07-16 14:56:40 +0700

    add the CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,13 @@ import logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/posts1.jpg";
 import PostProfileImage from "../assets/posts/posts-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
 const images = {
   logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
+  CtaImage,
 };
 
 export default images;
