d56525b6a61abde44971b8728d82d8e99e5e910f
rizan1998 <rizanalfalah@gmail.com>
2023-10-04 22:29:00 +0700

    adding the colom comment

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CommentForm from "./CommentForm";
 
 const CommentsContainer = ({ className }) => {
-  return <div className={`${className}`}></div>;
+  return (
+    <div className={`${className}`}>
+      <CommentForm btnLabel="Send" />
+    </div>
+  );
 };
 
 export default CommentsContainer;
