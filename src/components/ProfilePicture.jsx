525b62ba29994a7af87bdd3d7a4feb6b7fbec427
rizan1998 <rizanalfalah@gmail.com>
2024-02-10 17:56:40 +0700

    finish the cropped photo feature and delelte photo feature optimize header link direction

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -2,21 +2,60 @@ import React, { useState } from "react";
 import { HiOutlineCamera } from "react-icons/hi";
 
 import { stables } from "../constants";
-import CropEasy from "./crop/cropEasy";
+import CropEasy from "./crop/CropEasy";
 import { createPortal } from "react-dom";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateProfilePicture } from "../services/index/users";
+import { userActions } from "../store/reducers/userReducers";
 
 const ProfilePicture = ({ avatar }) => {
+  const userState = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
   const [openCrop, setOpenCrop] = useState(false);
   const [photo, setPhoto] = useState(null);
 
   const handleFileChange = (e) => {
-    const file = e.target.file[0];
-    setPhoto({
-      url: URL.createObjectURL(file),
-      file: file,
-    });
+    const file = e.target.files[0];
+    setPhoto({ url: URL.createObjectURL(file), file });
     setOpenCrop(true);
   };
+
+  const handleDeleteImage = async () => {
+    if (window.confirm("Do you want to delete your profile pricture")) {
+      try {
+        const formData = new FormData();
+        formData.append("profilePicture", undefined);
+
+        mutate({ token: userState.userInfo.token, formData: formData });
+      } catch (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    }
+  };
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }) => {
+      return updateProfilePicture({
+        token: token,
+        formData: formData,
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(userActions.setUserInfo(data));
+      setOpenCrop(false);
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile Photo is removed");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
 
   return (
     <>
@@ -33,8 +72,8 @@ const ProfilePicture = ({ avatar }) => {
             )}
           </label>
         </div>
-        <input type="file" className="sr-only" id="profilePicture" onChange={handleFileChange()} />
-        <button type="button" className="border border-red-500 rounded-lg px-4 py-2 text-red-500">
+        <input type="file" className="sr-only" id="profilePicture" onChange={handleFileChange} />
+        <button onClick={handleDeleteImage} type="button" className="border border-red-500 rounded-lg px-4 py-2 text-red-500">
           Delete
         </button>
       </div>
--- src/services/index/users.js
+++ src/services/index/users.js
@@ -34,6 +34,22 @@ export const updateProfile = async ({ token, userData }) => {
   }
 };
 
+export const updateProfilePicture = async ({ token, formData }) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.put("/api/users/updateProfilePicture", formData, config);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message) throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
 export const getUserProfile = async ({ token }) => {
   try {
     const config = {
