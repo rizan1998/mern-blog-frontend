c1fa8142f6db66f350ef07540792e5425dcd3966
rizan1998 <rizanalfalah@gmail.com>
2024-03-10 09:42:34 +0700

    show all the post data

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,12 +3,17 @@ import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/posts1.jpg";
 import PostProfileImage from "../assets/posts/posts-profile.svg";
 import CtaImage from "../assets/CtaImage.jpg";
+const sampelPostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
+
 const images = {
   logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
   CtaImage,
+  sampelPostImage,
+  userImage,
 };
 
 export default images;
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
+import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts } from "../../../services/index/posts";
 
 const Articles = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
-      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21PX)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21PX)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21PX)]" />
-      </div>
+      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">{!isLoading && !isError && data.map((post) => <ArticleCard key={post._id} post={post} className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21PX)]" />)}</div>
       <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
-        <span>More articles</span>
+        <span>More articles</span>s
         <FaArrowRight className="w-3 h-3" />
       </button>
     </section>
